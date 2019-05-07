0abf9aebdacb6f55efb54216f1166b2e97662819
Flukal <fluviobass@gmail.com>
2019-05-07 20:00:56 +0200

    Updates

---
+++ precache-manifest.b2c4a6b3ee41a92ad14d95eb97d36c62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073d99bedbc3420f508c227ec1f8a786",
+    "url": "/musicquiz/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/musicquiz/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8e8aefe70b477a04b764",
+    "url": "/musicquiz/static/js/main.1dc3bf3b.chunk.js"
+  },
+  {
+    "revision": "56d4ac220b30d8bf445d",
+    "url": "/musicquiz/static/js/runtime~main.95944bdf.js"
+  }
+]);
